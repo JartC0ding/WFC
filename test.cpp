ac8f874efbfa70eb306d8f43fe6774048506126e
MoritzSchittenhelm <78039560+JartC0ding@users.noreply.github.com>
2023-05-01 22:39:47 +0200

    better test.cpp

--- test.cpp
+++ test.cpp
@@ -1,24 +1,25 @@
 #include "src/include/wfc.hpp"
 #include <iostream>
 
-vector<int> res(5);
-
+// function for the first possible quantum
 bool c1(vector<int> &_res, int c_pos) {
-    if (_res[c_pos-1] == 0) return true;
+    if (_res[c_pos-1] == 0) return true; // if the previous was 0
     return false;
 }
 
+// function for the second possivle quantum
 bool c2(vector<int> &_res, int c_pos) {
-    if (_res[c_pos-1] == 1) return true;
+    if (_res[c_pos-1] == 1) return true; // if the previous was 1
     return false;
 }
 
 int main(void) {
+    vector<int> res(5); // result vector
     cout << to_string(res.size()) << endl;
 
     WFC my_wfc(0, res);
-    my_wfc.options.push_back(&c1);
-    my_wfc.options.push_back(&c2);
+    my_wfc.options.push_back(&c1); // add quantum 1
+    my_wfc.options.push_back(&c2); // add quantum 2
     res = my_wfc.collapse();
 
     for (auto i : res) {
